18dfb96c214cec1bd36c309c8b532173377ceb2b
digi-monkey <digimonkey@protonmail.com>
2024-01-10 17:27:25 +0800

    feat: Thai lang

--- script/src/lib.rs
+++ script/src/lib.rs
@@ -1,6 +1,6 @@
 use js_sys::Reflect;
 use wasm_bindgen::prelude::*;
-use whatlang::{detect_script, Script};
+use whatlang::{detect_script, Script::Thai};
 
 #[wasm_bindgen]
 pub fn pre_validate(){
@@ -13,7 +13,7 @@ pub fn is_valid_event(event: JsValue) -> bool {
         if let Ok(content) = Reflect::get(obj, &JsValue::from_str("content")) {
             if let Some(content) = content.as_string() {
                 let info = detect_script(content.as_str()).unwrap();
-                if info.eq(&Script::Hiragana) || info.eq(&Script::Katakana){
+                if info.eq(&Thai){
                     return true;
                 }
            }
